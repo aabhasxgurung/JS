dcdddd4c228a3cf51ffb41100ff2fde1da7c803f
Axbhxs <aabhasgrg46@gmail.com>
2023-06-01 15:36:36 +0545

    second commit

--- project/project.js
+++ project/project.js
@@ -1,25 +1,44 @@
 const prompt = require('prompt-sync')();
 
-const deposit = () => {
-while (true) {
-    const depositAmount = prompt("Enter a deposit amount: ")
-    const numberDepositNumber = parseFloat(depositAmount);
+const ROWS = 3
+const COLS = 3
 
-    if (NaN(numberDepositNumber) || numberDepositNumber <= 0) {
-        console.log("Invalid deposit amount, Try again.");
-    }
-    else {
-        return numberDepositNumber
-    }
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
 }
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2,
+}
+
+
+
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ")
+        const numberDepositNumber = parseFloat(depositAmount);
+
+        if (NaN(numberDepositNumber) || numberDepositNumber <= 0) {
+            console.log("Invalid deposit amount, Try again.");
+        }
+        else {
+            return numberDepositNumber
+        }
+    }
 }
 
 
 const getNumberOfLines = () => {
     while (true) {
-        const lines = prompt("Enter the number of lines to bet on (1-3): ") 
+        const lines = prompt("Enter the number of lines to bet on (1-3): ")
         const numberOfLines = parseFloat(lines);
-    
+
         if (NaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
             console.log("Invalid number of lines, try again.");
         }
@@ -29,5 +48,43 @@ const getNumberOfLines = () => {
     }
 }
 
-const depositAmount = deposit()
-const numberOfLines = getNumberOfLines
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the total bet per line: ")
+        const numberBet = parseFloat(bet);
+
+        if (NaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again.");
+        }
+        else {
+            return numberBet;
+        }
+    }
+}
+
+const spin = () => {
+    const symbols = []
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+    const reels = [];
+    for (let i = 0; i < COLS; i++) {
+        reels.push([])
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
+}
+
+
+let balance = deposit()
+const numberOfLines = getNumberOfLines()
+const bet = getBet(balance, numberOfLines)
+
